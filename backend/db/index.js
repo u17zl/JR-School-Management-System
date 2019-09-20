41b89a1d0a7407d9be068434d22847495493e7e3
Tony Gao <gwjlshebei@gmail.com>
2019-09-21 00:17:37 +1000

    list assignment, add new and delete done

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 var mongoose = require("mongoose");
-mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true });
+mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true,  useUnifiedTopology: true });
 
 var db = mongoose.connection;
 db.on("error", function (error) {  
